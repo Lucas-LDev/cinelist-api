47fad1f38662cbb60ba9bccdbff4b9cad21dc9bd
Lucas-LDev <lucasunisinos0@gmail.com>
2025-09-25 17:48:32 -0300

    initial project setup

---
+++ src/test/java/com/cinelist/cinelist_api/CinelistApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cinelist.cinelist_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CinelistApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
